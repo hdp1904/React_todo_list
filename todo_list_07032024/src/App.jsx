110669537a2f388e08789a77fdee44a517e2f91f
Chen, Yong-Chang <y.c@gatech.edu>
2024-07-04 22:21:06 -0700

    todo-list-07042024

---
+++ todo_list_07032024/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const increase = () => {
+    setCount(count => count +1);
+  }
+
+  const decrease = () => {
+    setCount(count => count -1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
+  const addTodo = (e) => {
+    setTodo(e.target.value);
+  }
+
+  const addTodoList = () => {
+    setTodoList([...todoList, todo]);
+  }
+
+  const deleteItem = (e,index) => {
+    setTodoList(todoList.filter( i =>  i+index !== e.target.parentElement.id))
+  }
+
+  return (
+    <>
+    <div>
+      <button onClick={increase}>Addition</button>
+      <button onClick={decrease}>Subtraction</button>
+      <button onClick={reset}>Reset</button>
+      <p>The number is : {count}</p>
+    </div>
+    <div>
+      <input type="text" onChange={e=>addTodo(e)}/>
+      <button onClick={addTodoList}>Add To List</button>
+      {todoList.map((item,index)=> <li key={item+index} id={item+index}>
+        {item}
+        <button onClick={(e)=>deleteItem(e,index)}>Delete</button>
+      </li>
+      )}
+
+    </div>
+    </>
+  )
+}
+
+export default App
