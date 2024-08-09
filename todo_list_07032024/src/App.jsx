28fa1cf76321f62462582f8fcff121331802d043
hdp1904 <33026524+hdp1904@users.noreply.github.com>
2024-08-08 22:25:07 -0700

    Update App.jsx

--- todo_list_07032024/src/App.jsx
+++ todo_list_07032024/src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
 
